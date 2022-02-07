1ab9d81584be8c13672205006c92d3b645b004d1
Joe Spandrusyszyn <github@illandril.net>
2022-02-06 21:03:31 -0500

    Adjusted log styling

--- module/scripts/module.js
+++ module/scripts/module.js
@@ -3,7 +3,7 @@ export const NAME = "Illandril's Grid Labels";
 export const CSS_PREFIX = `${KEY}--`;
 
 const _log = (logFN, ...args) => {
-  logFN.apply(console, [`%c${NAME} |`, 'background-color: #4f0104; color: #fff; padding: 0.1em 0.5em;', ...args]);
+  logFN.apply(console, [`%c${NAME}`, 'background-color: #4f0104; color: #fff; padding: 0.1em 0.5em;', ...args]);
 };
 
 export const log = {
